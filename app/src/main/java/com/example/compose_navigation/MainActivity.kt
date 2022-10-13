1db05432ffd7c556f8302e3377fa2c9829c98d68
geo967 <geomathew967@gmail.com>
2022-10-13 12:12:12 +0530

    c1

---
+++ app/src/main/java/com/example/compose_navigation/MainActivity.kt
@@ -0,0 +1,43 @@
+        package com.example.compose_navigation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.compose_navigation.ui.theme.ComposeNavigationTheme
+
+class MainActivity : ComponentActivity() {
+
+    lateinit var navController: NavHostController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeNavigationTheme {
+                navController = rememberNavController()
+                SetupNavGraph(navController = navController)
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeNavigationTheme {
+        Greeting("Android")
+    }
+}
